1318edbaa0452d7fb3f73e924914943a14cb7437
CindyBarria <cindybarria@gmail.com>
2020-04-07 10:17:48 -0400

    Avance primer proyecto

--- src/index.js
+++ src/index.js
@@ -1,3 +1,44 @@
-import validator from './validator.js';
+// Ocultar todo al cargar pagina
+document.getElementById("Portada").style.display = 'block';
+document.getElementById("Validador").style.display = 'none';
+document.getElementById("TarjetaValida").style.display = 'none';
+document.getElementById("TarjetaNoValida").style.display = 'none';
+//variables
+let nombre= document.getElementById ("wname");
+let correo= document.getElementById ("email");
 
-console.log(validator);
+
+
+
+
+//funcion y condicion para cambiar de pagina y traer nombre
+
+function siguiente() {
+// condicion si tiene menos de 0 letras dara un alerta 
+if (nombre.value.length > 0 && correo.value.length > 0){ 
+var usuario= document.getElementById("wname").value;
+document.getElementById("names").innerHTML="Bienvenid@ "+ usuario;
+//ocultar y mostrar  
+  document.getElementById("Portada").style.display = 'none';
+  document.getElementById("Validador").style.display='block';
+ }
+else{
+   alert( "Ingrese los datos por favor")
+    }
+}
+
+// Funciones para cambiar de pagina
+
+
+function volverVal() {
+  document.getElementById("TarjetaValida").style.display = 'none';
+  document.getElementById("Portada").style.display='block';
+};
+
+function volverNoValido() {
+  document.getElementById("TarjetaNoValida").style.display = 'none';
+  document.getElementById("Portada").style.display='block';
+};
+
+
+
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,58 @@
-const validator = {
-  // ...
-};
 
-export default validator;
+function isValidCard() {
+  //variables para evaluar numero ingresado y crear primer array
+  var numero = document.getElementById("numberCard").value;
+  var cadena = numero.split("");
+  var cadenaTotal = []; // Todos los numeros pares e impares
+  var mayoresDiez = []; 
+  var menoresDiez = [];
+  // for para separar numeros por posicion y multiplicar los numero en posicion par por 2
+for (i=0; i < cadena.length; i++) {
+  let valor = parseInt (cadena[i]); //cadena number
+  if (i % 2 == 0) {
+  var resultado = valor  
+  console.log ("impar i="+ i + " valor=" + valor );
+  } //valores impar 
+  else {
+  var resultado = valor*2  
+  console.log ("pares i="+ i + " valor=" + resultado)
+  } //posiciones pares multiplicadas por 2 
+  cadenaTotal.push(resultado);
+}
+  console.log(cadenaTotal);
+
+//cadena de numeros menos a diez
+for (let i = 0; i<cadenaTotal.length; i++) {
+  var resp = cadenaTotal[i];
+  if (resp < 10) {
+  menoresDiez.push(resp)
+  }
+}  
+  console.log(menoresDiez)
+  
+  //Cadena de numeros mayores a diez
+for (let i = 0; i<cadenaTotal.length; i++) {
+  var res = cadenaTotal[i];
+  if (res > 10) {
+  mayoresDiez.push(res)
+  }
+}
+  console.log(mayoresDiez)
+
+//separar numeros mayores a 10 
+  var mayorSeparados = [];
+  var digitos = mayoresDiez.toString();
+for (i = 0; i < digitos.length ; i++) {
+  mayorSeparados.push(parseInt(digitos.charAt(i)));
+}
+  console.log(mayorSeparados);
+
+//sumas finales
+const arrayFinal = menoresDiez.concat(mayorSeparados);
+  console.log (arrayFinal)
+
+  let total = arrayFinal.reduce((a, b) => a + b, 0);
+
+  console.log(total);
+
+};
